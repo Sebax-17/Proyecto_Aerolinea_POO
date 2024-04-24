7a772163f4bf0b6f67d23bf8822d946ff594a397
Montoya__ <126628157+Sebax-17@users.noreply.github.com>
2024-04-24 15:48:05 -0500

    intento#10

--- Program.js
+++ Program.js
@@ -9,19 +9,19 @@ class Reserva {
 } 
 let reser1 = new Reserva()
 reser1.Destino =`Cartagena`
-reser1.Impuesto = readlineSync.question(`Cuanto es el impuesto para:  ${reser1.Destino  }: `)
+reser1.Impuesto = +readlineSync.question(`Cuanto es el impuesto para:  ${reser1.Destino  }: `)
 reser1.Costo =250000 + (reser1.Costo*reser1.Impuesto )
 reser1.NombrePasajero = `Juan`
  
 let reser2 = new Reserva()
 reser2.Destino =`Miami`
-reser2.Impuesto = readlineSync.question(`Cuanto es el impuesto para ${reser2.Destino  }: `)
+reser2.Impuesto = +readlineSync.question(`Cuanto es el impuesto para ${reser2.Destino  }: `)
 reser2.Costo =700000 + (reser2.Costo*reser2.Impuesto)
 reser2.NombrePasajero = `Camila`
 
 let reser3 = new Reserva()
 reser3.Destino =`Alemania`
-reser3.Impuesto = readlineSync.question(`Cuanto es el impuesto para ${reser3.Destino  }: `)
+reser3.Impuesto = +readlineSync.question(`Cuanto es el impuesto para ${reser3.Destino  }: `)
 reser3.Costo =2000000 + (reser3.Costo*reser3.Impuesto)
 reser3.NombrePasajero =`Montoya`
 
