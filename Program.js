c4bcd2cdbc25de65819a54479b7bad70a2064f6a
Montoya__ <126628157+Sebax-17@users.noreply.github.com>
2024-04-25 19:08:02 -0500

    Intento #14

--- Program.js
+++ Program.js
@@ -1,35 +1,23 @@
 const readlineSync = require(`readline-sync`)
-class Reserva {
+class Aerolinea {
+    Vuelo = ``
+    Impuesto = 0 // es segun el destino Por ejemplo si es para san andres tiene un impuesto de x y por otro vuelo es de y
+    ImpNiños = 0
     
-    Destino=``;
-    NombrePasajero = ``
-    Costo = null
-    Impuesto = null
-    Origen = ``
+    Descuentos = 0.1
+    ImpDescuento = 0
+    //Metodo
+    ImpuestoMascota(){
+        CanMascotas= +readlineSync.question(`Cuantas Mascotas Va llevar en el vuelo. `)
+        ImPMascota = +readlineSync.question(`Cual es el impuesto por mascota?`)
+        ImpTotalMascotas = CanMascotas*(ImPMascota/100)
+        console.log(`El impuesto total por llevar mascotas es: ${ImpTotalMascotas}`)
+    }
 
 } 
-console.log(this.Impuesto)
-let reser1 = new Reserva()
-reser1.Origen = readlineSync.question(`Desde que ciudad o pais van a viajar? `)
-reser1.Destino =`Cartagena`
-reser1.Impuesto = +readlineSync.question(`Cuanto es el impuesto para:  ${reser1.Destino  }: `)
-reser1.Costo =250000 + (reser1.Costo*this.Impuesto )
-reser1.NombrePasajero = `Juan`
- 
-let reser2 = new Reserva()
-reser2.Origen = readlineSync.question(`Desde que ciudad o pais van a viajar? `)
-reser2.Destino =`Miami`
-reser2.Impuesto = +readlineSync.question(`Cuanto es el impuesto para ${reser2.Destino  }: `)
-reser2.Costo = 700000 
-reser2.NombrePasajero = `Camila`
-
-let reser3 = new Reserva()
-reser3.Origen = readlineSync.question(`Desde que ciudad o pais van a viajar? `)
-reser3.Destino =`Alemania`
-reser3.Impuesto = +readlineSync.question(`Cuanto es el impuesto para ${reser3.Destino  }: `)
-reser3.Costo = 2000000 + (reser3.Costo * this.Impuesto)
-reser3.NombrePasajero =`Montoya`
-
-console.log(reser1)
-console.log(reser2)
-console.log(reser3)
+class Reserva extends Aerolinea{
+    Origen = ``
+    Destino = ``
+    
+}
+this.ImpuestoMascota()
