6d459dd7bf5b0251cb9428de94f948cb7b9d8143
Montoya__ <126628157+Sebax-17@users.noreply.github.com>
2024-04-26 16:23:24 -0500

    25

--- Program.js
+++ Program.js
@@ -1,51 +1,32 @@
 const readlineSync = require(`readline-sync`)
-class Aerolinea {
-    Impuesto = 0 // es segun el destino Por ejemplo si es para san andres tiene un impuesto de x y por otro vuelo es de y
-    DulcesEntregadosaNiños = 0
-    CanDeNIños = 0
-    #Descuentos = 0.1
-    ImpDescuento = 0
-    //Metodo
-    ImpuestoMascota(){
-        CanMascotas= +readlineSync.question(`Cuantas Mascotas Va llevar en el vuelo. `)
-        ImPMascota = +readlineSync.question(`Cual es el impuesto por mascota?`)
-        ImpTotalMascotas = CanMascotas*(ImPMascota/100)
-        console.log(`El impuesto total por llevar mascotas es: ${ImpTotalMascotas}`)
+class Vuelo {
+    constructor(Origen,Destino,costo,impuesto){
+    this.Origen = Origen
+    this.Destino = Destino
+    this.costo = costo
+    this.impuesto = impuesto
     }
-    Dulces(){
-        console.info(`Los dulces solo son para niños menores de 12 años`)
-        CanDeNiños = readlineSync.question(`Cuantos niños infantes van a ir `)
-        while (CanDeNIños > Dulces){
-            this.DulcesEntregadosaNiños += 1
+    preciototal(){
+        Precio = this.costo +(this.costo*this.impuesto)/100
+    }
+}
+class Pasajero  {
+    dulce = 0
+    construtor(nombre,edad,tipodepasajero,vuelo){
+        this.nombre = nombre
+        this.edad = edad
+        this.tipodepasajero = tipodepasajero
+        this.vuelo = vuelo
+
+    }
+    PrecioPasaje(){
+        if(this.tipodepasajero == `mascota`){
+            this.costo = this.costo * 0.15
+        }
+        else if(this.tipodepasajero == `infante`) {
+            dulce += dulce + 1
         }
     }
-} 
-class Reserva extends Aerolinea{
-    Origen = ``
-    Destino = ``
-
-    Menu(){
-        console.log(`------ Destinos Disponibles -------`)
-        this.Destino = readlineSync.question(`Bogota(B) , Miami(M , Pereira(P))  `)
-
-    }
-    CostoVuelo(){
-        this.costo = +readlineSync.question(`Cual es valor del vuelo, ${this.Destino}`)
-        
-    }
-   ImpuestoDestino(Destino){
-    
-    this.Impuesto= +readlineSync.question(`Ingrese el impuesto de ${Destino}`)
-     
-
-   } 
-   CostoTotal(){
-    CostoFinal = this.costo + (this.Impuesto/100)
-   }
-   
 }
-let reser1 = new Reserva()
+let reser1 = new Pasajero();
 console.log(reser1)
-reser1.Menu()
-reser1.Dulces()
-reser1.ImpuestoDestino()
