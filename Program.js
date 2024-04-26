1ea26acb28e8caec94f23921fad1ab1a505c9356
Montoya__ <126628157+Sebax-17@users.noreply.github.com>
2024-04-26 15:24:34 -0500

    25

--- Program.js
+++ Program.js
@@ -14,7 +14,7 @@ class Aerolinea {
     }
     Dulces(){
         console.info(`Los dulces solo son para niños menores de 12 años`)
-        CanDeNIños = readlineSync.question(`Cuantos niños infantes van a ir `)
+        CanDeNiños = readlineSync.question(`Cuantos niños infantes van a ir `)
         while (CanDeNIños > Dulces){
             this.DulcesEntregadosaNiños += 1
         }
